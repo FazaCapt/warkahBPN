1499ba9220a5e93b4a334233475692045c839fae
Faza Fahamsyah <fazafahamsyah@Macbooks-MacBook-Pro.local>
2018-10-25 13:26:17 +0700

    Creating Our Pages: Membuat Nav Bar dan Link Route / Route

---
+++ app/components/Weather.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Weather = React.createClass({
+    render: function(){
+        return (
+            <h2>Weather Components</h2>
+        )
+    }
+});
+
+module.exports = Weather;
