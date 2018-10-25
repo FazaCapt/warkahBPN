0cf9bb387aaf6c3d5d28007667b7ac36eb43e7f6
Faza Fahamsyah <fazafahamsyah@Macbooks-MacBook-Pro.local>
2018-10-25 12:51:11 +0700

    Adding React-Router: Membuat file Main dan Nav, gimana caranga membuat beberapa component dalam 1 halaman/file

--- app/app.jsx
+++ app/app.jsx
@@ -1,16 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router'); 
-// Sama halnnya seperti dibawah 
-var Route = require('react-router').Route;
-// Penjelasan diatas seperti ini....
-var objOne = {
-    name: 'Faza'
-}
-var {name} = objOne;
+var Main = require('Main');
 
 
 ReactDOM.render(
-    <h1>BoilerPlate WarkahBPN</h1>,
+    <Router history={hashHistory}>
+        <Route path="/" component={Main}>
+
+        </Route>
+    </Router>,
     document.getElementById('app')
 );
