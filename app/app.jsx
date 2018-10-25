7391fd499b83abf9fdb7c793de9a4aaa8578c948
Faza Fahamsyah <fazafahamsyah@Macbooks-MacBook-Pro.local>
2018-10-25 12:16:50 +0700

    Routing: Instal Npm React-Route | Dan menjelaskan cara penulisan require dengan tanda kurung dengan contoh penjabarannya

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-
+var {Route, Router, IndexRoute, hashHistory} = require('react-router'); 
+// Sama halnnya seperti dibawah 
+var Route = require('react-router').Route;
+// Penjelasan diatas seperti ini....
+var objOne = {
+    name: 'Faza'
+}
+var {name} = objOne;
 
 
 ReactDOM.render(
