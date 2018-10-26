59be160ad490917be89ac6c4db5da0264c65c7e0
Faza Fahamsyah <fazafahamsyah@Macbooks-MacBook-Pro.local>
2018-10-26 20:56:09 +0700

    Making our api call: mengambil data dengan axios tapi dalam mengases izin api masih error harus diperbaiki tapi di skip aja dulu

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(8080, function(){
-    console.log('express server is up on port 8080');
+app.listen(3333, function(){
+    console.log('express server is up on port 3333');
 
 })
