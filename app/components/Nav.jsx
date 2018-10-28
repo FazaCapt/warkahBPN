5665d974ef028582c133e930a3ad4bdf94bb7b66
Faza Fahamsyah <fazafahamsyah@Macbooks-MacBook-Pro.local>
2018-10-28 21:50:03 +0700

    Styling Nav| Lihat web foundation zurb bila mau biat topbar. dan bila ada proses data atau pengambilan data tidak boleh stateless component harus pake react

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,14 +1,52 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = (props) => {
-    return (
-        <div>
-            <h2>Nav Components</h2>
-            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}} >Get Weather</IndexLink>
-            <Link to="/About" activeClassName="active" activeStyle={{fontWeight: 'bold'}} >About</Link>
-            <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}} >Examples</Link>
+var Nav = React.createClass({
+    onSearch: function(e){
+        e.preventDefault();
+        alert('Not Yet Wired up!');
+    },
+    render: function(){
+      return(
+        <div className="top-bar">
+        <div className="top-bar-left">
+            <ul className="menu">
+                <li className="menu-text">
+                    Warkah BPN
+                </li>
+                <li>
+                    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}} >Get Weather</IndexLink>
+                </li>
+                <li>
+                    <Link to="/About" activeClassName="active" activeStyle={{fontWeight: 'bold'}} >About</Link>
+                </li>
+                <li>
+                <Link to="/Examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}} >Examples</Link>
+                </li>
+            </ul>
+
         </div>
-    )
-}
+            <div className="top-bar-right">
+                <form onSubmit={this.onSearch}>
+                    <ul className="menu">
+                        <li>
+                        <input type="search" placeholder="Search Warkah"/>
+                        </li>
+                        <li>
+                        <input type="submit" className="button" value="Get Warkah"/>
+                        </li>
+                    </ul>
+                </form>
+        </div>
+    </div>
+
+      )  
+        
+    }
+})
+
 module.exports = Nav;
+            
+            
+            
+     
